520eafd4654248dfc4ce81ea61b175ee46f58859
Mapie Mamunhe <mmamunhe@gmail.com>
2024-09-02 14:05:59 +0200

    Add projects dynamically

--- js/portfolio-projects.js
+++ js/portfolio-projects.js
@@ -1,6 +1,17 @@
 import {addProjectAndReinitialize} from "../script.js";
   
-  // Example usage:
+addProjectAndReinitialize({
+    title: "Plataforma de Pagamentos Escrow (PAGOW)",
+    description: "Desenvolvi um sistema usando React Native para o frontend e ExpressJS para o backend, aplicando técnicas de testes automatizados com Jest. Este projeto foi a conclusão do meu curso e demonstra habilidades em desenvolvimento mobile e backend, com ênfase em testes para garantir a qualidade do software.",
+    technologies: ["React Native", "ExpressJS", "Jest"],
+    projectUrl: "https://pagow.vercel.app/",
+    sourceInfo: "github",
+    sourceCodeUrl: "https://github.com/MapieMamunhe/pagow",
+    imageUrl: "assets/images/work/final-project.png",
+    imageAlt: "Screenshot for Final Project"
+  });
+  
+  
   addProjectAndReinitialize({
     title:"Sistema de gestão de reservas",
     description:"Sistema de gestão de reservas de quartos de hotel: Gerir facilmente quartos e clientes com "+
@@ -14,3 +25,26 @@ import {addProjectAndReinitialize} from "../script.js";
     imageUrl: "assets/images/work/reservation-system.png",
     imageAlt: "Screenshot for Reservation System"
   });
+
+  addProjectAndReinitialize({
+    title: "Sistema de Vendas",
+    description: "Um sistema de vendas personalizável que aprimora suas operações com uma interface intuitiva e relatórios semanais detalhados. Adaptável aos seus fluxos de trabalho específicos, utiliza AWS, Spring Boot, AngularJS, Docker e Java para otimizar processos e auxiliar na tomada de decisões informadas.",
+    technologies: ["Hospedagem - AWS EC2", "Spring Boot", "AngularJS", "Docker", "Java"],
+    // projectUrl: "aaa",
+    // sourceInfo: "linkedin",
+    // sourceCodeUrl: "aaa",
+    imageUrl: "assets/images/work/sale-system.png",
+    imageAlt: "Screenshot for Sales System"
+  });
+  
+  addProjectAndReinitialize({
+    title: "API de Loteria",
+    description: "Desenvolvi uma API robusta em Node.js com TypeScript para gerenciamento de loterias. Suporta criação e consulta de palpites e resultados, com autenticação JWT segura. A documentação é gerada automaticamente pelo Swagger e a validação é feita com Zod para garantir a integridade dos dados. Utiliza SQL Server para armazenamento e é hospedada em um VPS, oferecendo alta disponibilidade e controle total do ambiente.",
+    technologies: ["Node.js", "TypeScript", "JWT", "Swagger", "Zod", "SQL Server", "Hospedagwm VPS"],
+    // projectUrl: "aaa",
+    // sourceInfo: "github",
+    // sourceCodeUrl: "aaa",
+    imageUrl: "assets/images/work/lottery-api.png",
+    imageAlt: "Screenshot for Lottery API"
+  });
+  
